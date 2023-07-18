905a7466d679e6809908a05220570b9cc346ccf2
unknown <janislacis06@gmail.com>
2023-07-18 22:35:06 +0300

    Add /newtrade route + minor changes

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const FacebookStrategy = require("passport-facebook").Strategy
 const TwitterStrategy = require("passport-twitter").Strategy
 const findOrCreate = require("mongoose-findorcreate")
 const session = require("express-session")
+const {default: axios} = require("axios")
 
 const saltRounds = 10
 
@@ -19,7 +20,12 @@ app.use((req, res, next) => {
     next()
 })
 
-app.use(cors())
+app.use(
+    cors({
+        origin: ["http://localhost:5173"],
+        methods: "GET,POST,PUT,DELETE,OPTIONS",
+    })
+)
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 mongoose.connect("mongodb://0.0.0.0:27017/tradingDB")
@@ -48,6 +54,7 @@ const userSchema = new mongoose.Schema({
     username: String,
     email: String,
     password: String,
+    profile: Object,
 })
 userSchema.plugin(findOrCreate)
 
@@ -59,14 +66,17 @@ passport.use(
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
             callbackURL: "http://localhost:3000/auth/google/callback",
+            userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
         },
-        function (accessToken, refreshToken, profile, cb) {
+        function (req, accessToken, refreshToken, profile, cb) {
             User.findOrCreate(
                 {
                     googleId: profile.id,
                     username: profile.displayName,
+                    email: profile.emails[0].value,
                 },
                 function (err, user) {
+                    req = profile.id
                     return cb(err, user)
                 }
             )
@@ -95,20 +105,20 @@ passport.use(
     )
 )
 
-passport.use(
-    new TwitterStrategy(
-        {
-            consumerKey: process.env.TWITTER_CLIENT_ID,
-            consumerSecret: process.env.TWITTER_CLIENT_SECRET,
-            callbackURL: "http://localhost:3000/auth/twitter/callback",
-        },
-        function (token, tokenSecret, profile, cb) {
-            User.findOrCreate({twitterId: profile.id}, function (err, user) {
-                return cb(err, user)
-            })
-        }
-    )
-)
+// passport.use(
+//     new TwitterStrategy(
+//         {
+//             consumerKey: process.env.TWITTER_CLIENT_ID,
+//             consumerSecret: process.env.TWITTER_CLIENT_SECRET,
+//             callbackURL: "http://localhost:3000/auth/twitter/callback",
+//         },
+//         function (token, tokenSecret, profile, cb) {
+//             User.findOrCreate({twitterId: profile.id}, function (err, user) {
+//                 return cb(err, user)
+//             })
+//         }
+//     )
+// )
 
 app.get("/api", (req, res) => {
     res.json({message: "hello"})
@@ -130,6 +140,19 @@ app.post("/api/login", (req, res) => {
             res.json({message: "user does not exist"})
         }
     })
+    // const user = new User({
+    //     email: req.body.email,
+    //     password: req.body.password,
+    // })
+    // req.login(user, (err) => {
+    //     if (err) {
+    //         console.log(err)
+    //     } else {
+    //         passport.authenticate("local")(req, res, () => {
+    //             res.json(user)
+    //         })
+    //     }
+    // })
 })
 
 app.post("/api/signup", (req, res) => {
@@ -163,23 +186,28 @@ app.post("/api/signup", (req, res) => {
         })
 })
 
-app.get("/api/login/google", (req, res) => {
-    res.redirect("http://localhost:3000/auth/google")
-})
-app.get("/auth/google", passport.authenticate("google", {scope: ["profile"]}))
+app.get("/auth/google", passport.authenticate("google", {scope: ["email"]}))
 app.get(
     "/auth/google/callback",
     passport.authenticate("google", {
         failureRedirect: "http://localhost:5173",
     }),
     function (req, res) {
-        res.redirect("http://localhost:5173/signup")
+        const id = req.user.googleId
+        // req.login(id, (err) => {
+        //     if (err) {
+        //         console.log(err)
+        //     } else {
+        //         passport.authenticate("local")(req, res, () => {
+        //             res.json({message: "hello"})
+        //         })
+        //     }
+        // })
+        // res.redirect("http://localhost:5173/dashboard")
+        res.send(id)
     }
 )
 
-app.get("/api/login/facebook", (req, res) => {
-    res.redirect("http://localhost:3000/auth/facebook")
-})
 app.get("/auth/facebook", passport.authenticate("facebook"))
 app.get(
     "/auth/facebook/callback",
@@ -191,19 +219,20 @@ app.get(
     }
 )
 
-app.get("/api/login/twitter", (req, res) => {
-    res.redirect("http://localhost:3000/auth/twitter")
+// app.get("/auth/twitter", passport.authenticate("twitter"))
+// app.get(
+//     "/auth/twitter/callback",
+//     passport.authenticate("twitter", {
+//         failureRedirect: "http://localhost:5173",
+//     }),
+//     function (req, res) {
+//         res.redirect("http://localhost:5173")
+//     }
+// )
+
+app.post("/api/newtrade", (req, res) => {
+    console.log(req.body)
 })
-app.get("/auth/twitter", passport.authenticate("twitter"))
-app.get(
-    "/auth/twitter/callback",
-    passport.authenticate("twitter", {
-        failureRedirect: "http://localhost:5173",
-    }),
-    function (req, res) {
-        res.redirect("http://localhost:5173")
-    }
-)
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
